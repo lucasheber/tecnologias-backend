c1d5bd38d5f042f644135d3563c816b460e6c4d8
Lucas Heber <lucas.heber07@gmail.com>
2023-05-22 18:19:36 -0300

    iniciando aula 01

---
+++ aula01/index.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+const HOSTNAME = '127.0.0.1';
+const PORT = 3000;
+
+http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-type': 'text/plain' });
+    res.end('Hello World');
+}).listen(PORT, HOSTNAME, () => {
+    console.log(`O servidor está sendo executando em ${HOSTNAME}:${PORT}`);
+});
