dc51e26bb3148927cd18c530539b88902a065792
Lucas Heber <lucas.heber07@gmail.com>
2023-05-29 19:52:52 -0300

    aula 02

--- aula02/server.js
+++ aula02/server.js
@@ -2,7 +2,6 @@ const http = require('http')
 const url = require('url');
 const fs = require('fs');
 const path = require('path');
-const { log } = require('console');
 
 const port = 3003;
 const hostname = 'localhost';
